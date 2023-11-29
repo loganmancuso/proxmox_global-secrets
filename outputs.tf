ee97024bd4990eb0d1b4f1d1ceaa2e8a85e945df
Logan Mancuso <mickeyacejr@live.com>
2023-11-28 20:20:21 -0500

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -6,13 +6,13 @@
 ##############################################################################
 
 # Shared Secrets #
-output "vault_path_instance_credentials" {
+output "vault_shared_instance_credentials" {
   description = "path in vault to instance_credentials secret"
-  value       = vault_kv_secret_v2.instance_credentials.path
+  value       = vault_kv_secret_v2.instance_credentials.name
 }
 
 # Infra Secrets
-output "vault_path_k8_postgres" {
+output "vault_infra_k8_postgres" {
   description = "path in vault to k8_postgres secret"
-  value       = vault_kv_secret_v2.k8_postgres.path
+  value       = vault_kv_secret_v2.k8_postgres.name
 }
